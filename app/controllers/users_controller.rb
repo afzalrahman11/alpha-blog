8092f1b7e1931027377f51401e157fe0cc1cac27
Afzal Rahman <afzalrahman123@gmail.com>
2021-07-09 19:31:03 +0530

    Add login and logout  seperation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update]
+
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -9,14 +10,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
     def index
         @users = User.paginate(page: params[:page], per_page: 4)
     end
     def update
-        @user = User.find(params[:id])
-
         if @user.update(user_params)
             flash[:notice] = "Your account has been successfully updated."
             redirect_to @user
@@ -29,6 +27,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
 			flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have been successfully signed in."
 			redirect_to @user
 		else
@@ -41,4 +40,9 @@ class UsersController < ApplicationController
     def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 end
