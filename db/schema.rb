29cad5f7c50f409e84977a86a2838665fd85d60d
Afzal Rahman <afzalrahman123@gmail.com>
2021-07-14 13:04:56 +0530

    Add article category many-to-many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_071353) do
+ActiveRecord::Schema.define(version: 2021_07_14_072020) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "article_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
